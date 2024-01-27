b0eef0ae7c11ff0d766f3021cbbc6583d40e0fdf
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-01-27 19:23:56 +0200

    idk wtf that was but it works

--- src/middlewares/paginationQueryParams.middleware.ts
+++ src/middlewares/paginationQueryParams.middleware.ts
@@ -8,43 +8,43 @@ import { NextFunction, Request, Response } from "express";
  * @returns `400 Bad Request` if the query parameters are missing or invalid
  */
 const middlewares = [
-validator
-  // Get the `page` query parameter
-  .query("page")
-
-  // Ensure that the `page` query parameter exists
-  .exists()
-      ithMessage("Missing query parameter page")
-      
-    // Ensure that the `page` query parameter is an integer
-      .isInt({ min: 1 })
-      .withMessage("Query parameter page must be an integer greater than 0"),
-      
   validator
-      // Get the `pageSize` query parameter
-      .query("pageSize")
-      
+    // Get the `page` query parameter
+    .query("page")
+
+    // Ensure that the `page` query parameter exists
+    .exists()
+    .withMessage("Missing query parameter page")
+
+    // Ensure that the `page` query parameter is an integer
+    .isInt({ min: 1 })
+    .withMessage("Query parameter page must be an integer greater than 0"),
+
+  validator
+    // Get the `pageSize` query parameter
+    .query("pageSize")
+
     // Ensure that the `pageSize` query parameter exists
     .exists()
-      .withMessage("Missing query parameter pageSize")
+    .withMessage("Missing query parameter pageSize")
 
-      // Ensure that the `pageSize` query parameter is an integer
-      .isInt({ min: 1 })
+    // Ensure that the `pageSize` query parameter is an integer
+    .isInt({ min: 1 })
     .withMessage("Query parameter pageSize must be an integer greater than 0"),
-      
-        
-          Request, res: Response, next: NextFunction) => {
-            
-          f any of the validations above failed, return an error response
-        nst errors = validator.validationResult(req);
-      
+
+
+  (req: Request, res: Response, next: NextFunction) => {
+
+    // If any of the validations above failed, return an error response
+    const errors = validator.validationResult(req);
+
     if (!errors.isEmpty()) {
-        return res.status(400).json({
-          error: {
-            message: errors.array()[0].msg,
+      return res.status(400).json({
+        error: {
+          message: errors.array()[0].msg,
         },
-        });
-      }
+      });
+    }
 
     // Attach the query parameters to the context.
     req.context.page = parseInt(req.query.page as string);
